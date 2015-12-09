587f82d82e13358d076618a0f94a0c3a8eb24d57
RomaBlaSo <voloshyn.roman@gmail.com>
2015-12-09 22:19:25 +0200

    2015.12.09 22:20 (int task complete)

--- src/homeWeek1/MyInteger.java
+++ src/homeWeek1/MyInteger.java
@@ -17,11 +17,50 @@ public class MyInteger {
         value = number;
     }
 
-    public String toString(){return String.format("%d", value);}
+    public String toString() {
+        return String.format("%d", value);
+    }
 
-    public MyInteger add(int num){return new MyInteger(value + num);}
-    public MyInteger diff(int num){return new MyInteger(value - num);}
-    public MyInteger op(int num){return new MyInteger(value * num);}
-    public MyInteger division(int num){return new MyInteger(value / num);}
+    public MyInteger add(MyInteger num) {
+        return new MyInteger(value + num.value);
+    }
+
+    public MyInteger diff(MyInteger num) {
+        return new MyInteger(value - num.value);
+    }
+
+    public MyInteger op(MyInteger num) {
+        return new MyInteger(value * num.value);
+    }
+
+    public MyInteger division(MyInteger num) {
+        return new MyInteger(value / num.value);
+    }
+
+    public MyInteger restDiv(MyInteger num) {
+        return new MyInteger(value % num.value);
+    }
+
+    public MyInteger pow(MyInteger num) {
+
+        int res = 1;
+        for (int i = 1; i <= num.value; i++) {
+            res *= value;
+        }
+        return new MyInteger(res);
+    }
+
+    public MyInteger factorial() {
+
+        long res = 1;
+        for (int i = value; i > 1; i--) {
+            res *= i;
+        }
+        return new MyInteger((int) res);
+    }
+
+    public boolean equals(MyInteger num) {
+        return value == num.value;
+    }
 
 }
--- src/homeWeek1/TestTasks.java
+++ src/homeWeek1/TestTasks.java
@@ -4,8 +4,20 @@ public class TestTasks {
 
     public static void main(String[] args) {
 
-        MyInteger num1 = new MyInteger(100);
-        System.out.println(num1.add(25));
+        MyInteger numPow = new MyInteger(5);
+        MyInteger numFac = new MyInteger(5);
+        MyInteger num100 = new MyInteger(100);
+        MyInteger num25 = new MyInteger(25);
+        MyInteger num17 = new MyInteger(17);
+
+        System.out.printf("Sum %s + %s = %s%n", num100, num25, num100.add(num25));
+        System.out.printf("Diff %s - %s = %s%n", num100, num25, num100.diff(num25));
+        System.out.printf("Op %s * %s = %s%n", num100, num25, num100.op(num25));
+        System.out.printf("Division %s / %s = %s%n", num100, num17, num100.division(num17));
+        System.out.printf("Rest division %s + %s = %s%n", num100, num17, num100.restDiv(num17));
+        System.out.printf("Pow %s ^ %s = %s%n", num100, numPow, num100.pow(numPow));
+        System.out.printf("Factorial %s! = %s%n", numFac, numFac.factorial());
+        System.out.printf("Equals: %s %s %s", numFac, (numFac.equals(numPow) ? "equals" : "NOT equals"), numPow);
 
     }
 
